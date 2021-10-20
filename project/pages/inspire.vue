7dbc26b0ba602628404482d529357c69775207a7
LostGeneration1999 <masaya4426saganosen@gmail.com>
2021-10-20 13:30:42 +0900

    css学習中

--- project/pages/inspire.vue
+++ project/pages/inspire.vue
@@ -1,17 +1,76 @@
 <template>
   <v-row>
-    <v-col class="text-center">
-      <a href="https://nuxtjs.org">External Link to another page</a>
-      <NuxtLink to="/profile">Profileへ</NuxtLink>
-      <img src="/v.png" alt="Vuetify.js" class="mb-5" />
-      <blockquote class="blockquote">
-        &#8220;井上のさい&#8221;
-        <footer>
-          <small>
-            <em>&mdash;Masaya INoue</em>
-          </small>
-        </footer>
-      </blockquote>
-    </v-col>
+    <div class="white-outer">
+      <div class="black-box">
+        <div>
+          <h1>
+            Grateful Days
+            <span class="title">/Dragon Ash(1999)</span>
+          </h1>
+        </div>
+        <div class="lyric">
+          <p>俺は東京生まれHIPHOP育ち</p>
+          <p>悪そうな奴は大体友達</p>
+          <p>悪そうな奴と大体同じ</p>
+          <p>裏の道</p>
+          <p>歩き見てきたこの街</p>
+        </div>
+      </div>
+    </div>
   </v-row>
 </template>
+
+<style scoped lang="scss">
+/* これは CSS コメントです
+複数行に分割できます。 */
+
+/* || 一般的なスタイル */
+
+h1 {
+  font-size: 30px;
+  color: white;
+  border-bottom: 1px solid white;
+  text-align: center;
+}
+
+/* || ユーティリティ */
+
+.white-outer {
+  border: 5px solid white;
+}
+
+.lyric {
+  border: 2px solid white;
+  list-style: none;
+  display: block;
+  padding: 1em;
+  margin: 2em;
+}
+
+.lyric > p {
+  color: yellow;
+}
+
+.inline-box {
+  border: 2px solid white;
+}
+
+.block li {
+  border: 2px solid navy;
+  padding: 2em;
+}
+
+.black-box {
+  padding: 40px 40px 40px 40px;
+  margin: 0px 0px 0px 0px;
+  background-color: black;
+}
+
+.title {
+  font-size: 20px;
+}
+
+.block {
+  display: block;
+}
+</style>
--- project/pages/profile.vue
+++ project/pages/profile.vue
@@ -1,5 +1,87 @@
 <template>
   <v-row>
-    <NuxtLink to="/inspire">Inspureへ</NuxtLink>
+    <div class="outer">
+      <div class="cent">
+        <h1>私のプロフィール</h1>
+        <ul>
+          <li class="navy-color">野球好き</li>
+          <li>バットしない大学院生</li>
+        </ul>
+        <div class="right-text">
+          <a href="https://nuxtjs.org">next page</a>
+        </div>
+      </div>
+    </div>
+    <!-- <v-col cols="12" sm="8" md="6" lg="4" xl="3" style="background-color: #FFCDD2">column 1</v-col> -->
   </v-row>
 </template>
+
+<style scoped lang="scss">
+/* これは CSS コメントです
+複数行に分割できます。 
+以下のテンプレートごとに分割しましょう */
+
+/* || 一般的なスタイル */
+
+ul {
+  //   1emの大きさはブラウザーのfont-sizeのデフォルト値
+  margin: 3em 3em 3em 3em;
+}
+
+li {
+  font-size: 20px;
+  color: black;
+  text-align: left;
+  line-height: 2;
+  letter-spacing: 1px;
+}
+
+ul li:first-child {
+  font-weight: 700;
+}
+
+h1 {
+  font-size: 30px;
+  color: #375e97;
+  border-bottom: 1px solid #375e97;
+  text-align: center;
+}
+
+a::after {
+  content: " ➥";
+}
+
+a:link {
+  color: red;
+}
+
+a:visited {
+  color: orange;
+}
+
+a:hover {
+  text-decoration: none;
+}
+
+/* || ユーティリティ */
+
+/*  || サイトワイド */
+
+.outer {
+  border: 5px solid black;
+}
+
+.cent {
+  padding: 40px 40px 40px 40px;
+  margin: 0px 0px 0px 0px;
+  background-color: white;
+}
+
+.right-text {
+  text-align: right;
+}
+
+li.navy-color {
+  color: navy;
+}
+</style>
